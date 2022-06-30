480fe3d23679b465b83d290b72cc9356a02ba01c
Peder Hovdan Andresen <pederhan@usit.uio.no>
2022-06-30 12:18:09 +0200

    Remove non-iterable types from JSONType
    This simplifies type checking, as both dicts and lists are iterables.

--- harborapi/types.py
+++ harborapi/types.py
@@ -1,3 +1,7 @@
 from typing import Any, Dict, List, Union
 
-JSONType = Union[None, int, float, str, Dict[str, Any], List[Any]]
+JSONType = Union[Dict[str, Any], List[Any]]
+
+# This is more correct, but leads to a whole lot of typing bloat wrt. handling iterables.
+# We assume that the API will always return a list or dict.
+# JSONType = Union[None, int, float, str, Dict[str, Any], List[Any]]
