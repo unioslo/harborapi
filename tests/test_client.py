1a71bedf68fec41f02394992d466e98be6ac596f
Peder Hovdan Andresen <pederhan@usit.uio.no>
2022-06-30 14:35:50 +0200

    Add client tests

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,9 +1,10 @@
+import asyncio
+
 import pytest
-from hypothesis import HealthCheck, given, settings
-from hypothesis import strategies as st
+from pydantic import ValidationError
 from pytest_httpserver import HTTPServer
 
-from harborapi.client import HarborAsyncClient
+from harborapi.client import HarborAsyncClient, construct_model
 from harborapi.model import UserResp
 
 
@@ -37,17 +38,99 @@ def test_client_init_sanity():
 
 
 @pytest.mark.asyncio
-@given(st.builds(UserResp), st.builds(UserResp))
-@settings(suppress_health_check=[HealthCheck.function_scoped_fixture])
-async def test_get_users_mock(
+async def test_get_pagination_mock(
+    async_client: HarborAsyncClient, httpserver: HTTPServer
+):
+    """Test pagination by mocking paginated /users results."""
+    httpserver.expect_oneshot_request("/api/v2.0/users").respond_with_json(
+        [{"username": "user1"}, {"username": "user2"}],
+        headers={"link": "/users?page=2"},
+    )
+    httpserver.expect_request(
+        "/api/v2.0/users", query_string="page=2"
+    ).respond_with_json([{"username": "user3"}, {"username": "user4"}])
+    async_client.url = httpserver.url_for("/api/v2.0")
+    users = await async_client.get("/users")  # type: ignore
+    assert type(users) is list
+    assert len(users) == 4
+    assert users[0]["username"] == "user1"
+    assert users[1]["username"] == "user2"
+    assert users[2]["username"] == "user3"
+    assert users[3]["username"] == "user4"
+
+
+@pytest.mark.asyncio
+async def test_get_pagination_invalid_mock(
     async_client: HarborAsyncClient,
     httpserver: HTTPServer,
-    user1: UserResp,
-    user2: UserResp,
+    caplog: pytest.LogCaptureFixture,
 ):
-    httpserver.expect_request("/users").respond_with_json([user1.dict(), user2.dict()])
-    async_client.url = httpserver.url_for("")
-    users = await async_client.get_users()
+    """Test pagination where subsequent page returns non-list results."""
+    httpserver.expect_oneshot_request("/api/v2.0/users").respond_with_json(
+        [{"username": "user1"}, {"username": "user2"}],
+        headers={"link": "/users?page=2"},
+    )
+    # Next page does not return a list, so we ignore it
+    httpserver.expect_request(
+        "/api/v2.0/users", query_string="page=2"
+    ).respond_with_json({"username": "user3"})
+    async_client.url = httpserver.url_for("/api/v2.0")
+    users = await async_client.get("/users")  # type: ignore
+    assert type(users) is list
     assert len(users) == 2
-    assert users[0] == user1
-    assert users[1] == user2
+    assert users[0]["username"] == "user1"
+    assert users[1]["username"] == "user2"
+    assert "Unable to handle paginated results" in caplog.text
+
+
+@pytest.mark.asyncio
+async def test_get_retry_mock(async_client: HarborAsyncClient, httpserver: HTTPServer):
+    """Test retry by mocking a server that is initially down, then comes up."""
+    httpserver.stop()
+
+    # this is a little hacky for now:
+    # we schedule the server to start after a few seconds
+    async def start_server():
+        await asyncio.sleep(2)  # can be increased, but wastes CI run time
+        httpserver.start()
+
+    asyncio.create_task(start_server())
+
+    httpserver.expect_request("/api/v2.0/users").respond_with_json(
+        [{"username": "user1"}, {"username": "user2"}],
+    )
+
+    async_client.url = httpserver.url_for("/api/v2.0")
+    users = await async_client.get("/users")
+    assert type(users) is list
+    assert len(users) == 2
+
+
+@pytest.mark.asyncio
+async def test_construct_model():
+    # TODO: test create_model with all models
+    m = construct_model(UserResp, {"username": "user1"})
+    assert isinstance(m, UserResp)
+    assert m.username == "user1"
+
+    # Invalid value for "username"
+    with pytest.raises(ValidationError) as e:
+        construct_model(UserResp, {"username": {}})
+    assert e.value.errors()[0]["loc"] == ("username",)
+    assert len(e.value.errors()) == 1
+
+
+@pytest.mark.asyncio
+async def test_get_invalid_data(
+    async_client: HarborAsyncClient, httpserver: HTTPServer
+):
+    """Tests handling of data from the server that does not match the schema."""
+    httpserver.expect_request("/api/v2.0/users").respond_with_json(
+        [{"username": {}}, {"username": "user2"}],
+    )
+
+    async_client.url = httpserver.url_for("/api/v2.0")
+    with pytest.raises(ValidationError) as e:
+        await async_client.get_users()
+    assert e.value.errors()[0]["loc"] == ("username",)
+    assert len(e.value.errors()) == 1
