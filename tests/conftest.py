1c17286f730e140682039846060618d8a104ef7c
Peder Hovdan Andresen <pederhan@usit.uio.no>
2022-06-30 12:15:48 +0200

    Add loguru caplog fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,6 @@
 import pytest
+from _pytest.logging import LogCaptureFixture
+from loguru import logger
 
 from harborapi.client import HarborAsyncClient
 
@@ -6,3 +8,11 @@ from harborapi.client import HarborAsyncClient
 @pytest.fixture(scope="session")
 def async_client() -> HarborAsyncClient:
     return HarborAsyncClient(username="username", token="token", url="http://localhost")
+
+
+@pytest.fixture
+def caplog(caplog: LogCaptureFixture):
+    # https://loguru.readthedocs.io/en/stable/resources/migration.html#making-things-work-with-pytest-and-caplog
+    handler_id = logger.add(caplog.handler, format="{message}")
+    yield caplog
+    logger.remove(handler_id)
