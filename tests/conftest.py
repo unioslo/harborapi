b0509dd016142c9786a879ffb19a46f0324b9e9b
Peder Hovdan Andresen <pederhan@usit.uio.no>
2022-07-01 14:56:34 +0200

    Tests: Add hypothesis profiles

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,22 @@
+import os
+
 import pytest
 from _pytest.logging import LogCaptureFixture
+from hypothesis import Verbosity, settings
 from loguru import logger
 
 from harborapi.client import HarborAsyncClient
+
+settings.register_profile("ci", settings(max_examples=1000))
+settings.register_profile(
+    "debug",
+    settings(
+        max_examples=10,
+        verbosity=Verbosity.verbose,
+    ),
+)
+settings.register_profile("dev", max_examples=10)
+settings.load_profile(os.getenv("HYPOTHESIS_PROFILE", "default"))
 
 
 @pytest.fixture(scope="session")
