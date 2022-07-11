b0e941f5df7ae69342cfb5b1e97b4407d6f99238
Peder Hovdan Andresen <pederhan@usit.uio.no>
2022-07-11 10:30:42 +0200

    Tests: add `/users` tests

--- tests/endpoints/test_users.py
+++ tests/endpoints/test_users.py
@@ -7,25 +7,34 @@ from pytest_httpserver import HTTPServer
 
 from harborapi.client import HarborAsyncClient
 from harborapi.models import UserResp
-from harborapi.models.models import Permission, UserSearchRespItem
+from harborapi.models.models import (
+    PasswordReq,
+    Permission,
+    UserCreationReq,
+    UserProfile,
+    UserSearchRespItem,
+)
 
 from ..utils import json_from_list
 
 
 @pytest.mark.asyncio
+@pytest.mark.parametrize("sort", ["username", None, ""])
 @given(st.builds(UserResp), st.builds(UserResp))
 @settings(suppress_health_check=[HealthCheck.function_scoped_fixture])
 async def test_get_users_mock(
     async_client: HarborAsyncClient,
     httpserver: HTTPServer,
+    sort: Optional[str],
     user1: UserResp,
     user2: UserResp,
 ):
-    httpserver.expect_oneshot_request("/api/v2.0/users").respond_with_json(
-        [user1.dict(), user2.dict()]
-    )
+    q = f"sort={sort}" if sort else None
+    httpserver.expect_oneshot_request(
+        "/api/v2.0/users", method="GET", query_string=q
+    ).respond_with_json([user1.dict(), user2.dict()])
     async_client.url = httpserver.url_for("/api/v2.0")
-    users = await async_client.get_users()
+    users = await async_client.get_users(sort)
     assert len(users) == 2
     assert users[0] == user1
     assert users[1] == user2
@@ -57,3 +66,162 @@ async def test_get_users_by_username_mock(
     async_client.url = httpserver.url_for("/api/v2.0")
     resp = await async_client.get_users_by_username("username")
     assert resp == users
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "scope,relative,expected_query",
+    [
+        ("user", False, {"scope": "user", "relative": False}),
+        ("user", True, {"scope": "user", "relative": True}),
+        (None, False, {}),
+        (None, True, {}),
+        ("", True, {}),
+        ("", True, {}),
+    ],
+)
+@given(st.lists(st.builds(Permission)))
+@settings(suppress_health_check=[HealthCheck.function_scoped_fixture])
+async def test_get_current_user_permissions_mock(
+    async_client: HarborAsyncClient,
+    httpserver: HTTPServer,
+    scope: Optional[str],
+    relative: bool,
+    expected_query: dict,
+    permissions: List[Permission],
+):
+    # for some reason, pytest-httpserver will not match the query string
+    # when passing it as a dict, so we need to convert it to a string
+    query_string = "&".join(f"{k}={v}".lower() for k, v in expected_query.items())
+    httpserver.expect_oneshot_request(
+        "/api/v2.0/users/current/permissions",
+        method="GET",
+        query_string=query_string,
+    ).respond_with_data(json_from_list(permissions), content_type="application/json")
+    async_client.url = httpserver.url_for("/api/v2.0")
+    resp = await async_client.get_current_user_permissions(scope, relative)
+    assert resp == permissions
+
+
+@pytest.mark.asyncio
+@given(st.builds(UserResp))
+@settings(suppress_health_check=[HealthCheck.function_scoped_fixture])
+async def test_get_current_user_mock(
+    async_client: HarborAsyncClient,
+    httpserver: HTTPServer,
+    user: UserResp,
+):
+    httpserver.expect_oneshot_request(
+        "/api/v2.0/users/current",
+        method="GET",
+    ).respond_with_data(user.json(), content_type="application/json")
+    async_client.url = httpserver.url_for("/api/v2.0")
+    resp = await async_client.get_current_user()
+    assert resp == user
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("is_admin", [True, False])
+async def test_set_user_admin_mock(
+    async_client: HarborAsyncClient,
+    httpserver: HTTPServer,
+    is_admin: bool,
+):
+    httpserver.expect_oneshot_request(
+        f"/api/v2.0/users/1234/sysadmin",
+        method="PUT",
+    ).respond_with_data()
+    async_client.url = httpserver.url_for("/api/v2.0")
+    await async_client.set_user_admin(1234, is_admin)
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "new_password,old_password",
+    (
+        # Using new and old password (regular user)
+        ["newpw123", "oldpw123"],
+        # Omitting the old password (admin)
+        ["newpw123", None],
+    ),
+)
+async def test_set_user_password_mock(
+    async_client: HarborAsyncClient,
+    httpserver: HTTPServer,
+    new_password: str,
+    old_password: Optional[str],
+):
+    # NOTE: This mock could stand to be expanded/improved
+    #       We are not mocking the distinction between user and admin behavior
+    httpserver.expect_oneshot_request(
+        f"/api/v2.0/users/1234/password",
+        method="PUT",
+        json=PasswordReq(new_password=new_password, old_password=old_password).dict(),
+    ).respond_with_data()
+    async_client.url = httpserver.url_for("/api/v2.0")
+    await async_client.set_user_password(1234, new_password, old_password)
+
+
+@pytest.mark.asyncio
+@given(st.builds(UserCreationReq))
+@settings(suppress_health_check=[HealthCheck.function_scoped_fixture])
+async def test_create_user_mock(
+    async_client: HarborAsyncClient,
+    httpserver: HTTPServer,
+    user: UserCreationReq,
+):
+    httpserver.expect_oneshot_request(
+        "/api/v2.0/users",
+        method="POST",
+        json=user.dict(),
+    ).respond_with_data(status=201, headers={"Location": "/api/v2.0/users/1234"})
+    async_client.url = httpserver.url_for("/api/v2.0")
+    resp = await async_client.create_user(user)
+    assert resp == "/api/v2.0/users/1234"
+
+
+@pytest.mark.asyncio
+@given(st.builds(UserProfile))
+@settings(suppress_health_check=[HealthCheck.function_scoped_fixture])
+async def test_update_user_profile_mock(
+    async_client: HarborAsyncClient,
+    httpserver: HTTPServer,
+    user: UserProfile,
+):
+    httpserver.expect_oneshot_request(
+        "/api/v2.0/users/1234",
+        method="PUT",
+        json=user.dict(),
+    ).respond_with_data(status=200)
+    async_client.url = httpserver.url_for("/api/v2.0")
+    await async_client.update_user_profile(1234, user)
+
+
+@pytest.mark.asyncio
+@given(st.builds(UserResp))
+@settings(suppress_health_check=[HealthCheck.function_scoped_fixture])
+async def test_get_user_mock(
+    async_client: HarborAsyncClient,
+    httpserver: HTTPServer,
+    user: UserResp,
+):
+    httpserver.expect_oneshot_request(
+        "/api/v2.0/users/1234",
+        method="GET",
+    ).respond_with_data(user.json(), content_type="application/json")
+    async_client.url = httpserver.url_for("/api/v2.0")
+    resp = await async_client.get_user(1234)
+    assert user == resp
+
+
+@pytest.mark.asyncio
+async def test_delete_user_mock(
+    async_client: HarborAsyncClient,
+    httpserver: HTTPServer,
+):
+    httpserver.expect_oneshot_request(
+        "/api/v2.0/users/1234",
+        method="DELETE",
+    ).respond_with_data()  # API responds with status 200, not 204
+    async_client.url = httpserver.url_for("/api/v2.0")
+    await async_client.delete_user(1234)
