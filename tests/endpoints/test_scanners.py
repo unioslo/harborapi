e3e899719484f116783cfc72d7ad00265fee25d8
Peder Hovdan Andresen <pederhan@usit.uio.no>
2022-07-11 10:49:28 +0200

    Tests: add remaining `/scanners` tests

--- tests/endpoints/test_scanners.py
+++ tests/endpoints/test_scanners.py
@@ -5,9 +5,11 @@ from pytest_httpserver import HTTPServer
 
 from harborapi.client import HarborAsyncClient
 from harborapi.models import (
+    IsDefault,
     ScannerAdapterMetadata,
     ScannerRegistration,
     ScannerRegistrationReq,
+    ScannerRegistrationSettings,
 )
 
 
@@ -41,10 +43,10 @@ async def test_get_scanners_mock(
         [scanner1.dict(), scanner2.dict()]
     )
     async_client.url = httpserver.url_for("/api/v2.0")
-    scanners = await async_client.get_scanners()
-    assert len(scanners) == 2
-    assert scanners[0] == scanner1
-    assert scanners[1] == scanner2
+    resp = await async_client.get_scanners()
+    assert len(resp) == 2
+    assert resp[0] == scanner1
+    assert resp[1] == scanner2
 
 
 @pytest.mark.asyncio
@@ -59,8 +61,8 @@ async def test_get_scanner_mock(
         scanner.dict()
     )
     async_client.url = httpserver.url_for("/api/v2.0")
-    scanner_rtn = await async_client.get_scanner(registration_id=1234)
-    assert scanner_rtn == scanner
+    resp = await async_client.get_scanner(registration_id=1234)
+    assert resp == scanner
 
 
 @pytest.mark.asyncio
@@ -75,5 +77,57 @@ async def test_get_scanner_metadata_mock(
         scannermeta.dict()
     )
     async_client.url = httpserver.url_for("/api/v2.0")
-    scannermeta_rtn = await async_client.get_scanner_metadata(registration_id=1234)
-    assert scannermeta_rtn == scannermeta
+    resp = await async_client.get_scanner_metadata(registration_id=1234)
+    assert resp == scannermeta
+
+
+@pytest.mark.asyncio
+@given(st.builds(ScannerRegistration))
+@settings(suppress_health_check=[HealthCheck.function_scoped_fixture])
+async def test_delete_scanner_mock(
+    async_client: HarborAsyncClient,
+    httpserver: HTTPServer,
+    scanner: ScannerRegistration,
+):
+    httpserver.expect_oneshot_request(
+        "/api/v2.0/scanners/1234", method="DELETE"
+    ).respond_with_data(scanner.json(), content_type="application/json")
+    async_client.url = httpserver.url_for("/api/v2.0")
+    resp = await async_client.delete_scanner(1234)
+    assert resp == scanner
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "is_default",
+    [True, False],
+)
+async def test_set_default_scanner_mock(
+    async_client: HarborAsyncClient,
+    httpserver: HTTPServer,
+    is_default: bool,
+):
+    httpserver.expect_oneshot_request(
+        "/api/v2.0/scanners/1234",
+        method="PATCH",
+        json=IsDefault(is_default=is_default).dict(),
+    ).respond_with_data()
+    async_client.url = httpserver.url_for("/api/v2.0")
+    await async_client.set_default_scanner(1234, is_default)
+
+
+@pytest.mark.asyncio
+@given(st.builds(ScannerRegistrationSettings))
+@settings(suppress_health_check=[HealthCheck.function_scoped_fixture])
+async def test_ping_scanner_adapter_mock(
+    async_client: HarborAsyncClient,
+    httpserver: HTTPServer,
+    settings: ScannerRegistrationSettings,
+):
+    httpserver.expect_oneshot_request(
+        "/api/v2.0/scanners/ping",
+        method="POST",
+        json=settings.dict(),
+    ).respond_with_data()
+    async_client.url = httpserver.url_for("/api/v2.0")
+    await async_client.ping_scanner_adapter(settings)
