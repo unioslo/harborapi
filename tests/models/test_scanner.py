230a2197430976ed86b5b083a2f7c91771244d2b
pederhan <pederhan@uio.no>
2022-08-12 11:05:08 +0200

    Add vulnerabilityitem.get_severity test

--- tests/models/test_scanner.py
+++ tests/models/test_scanner.py
@@ -1,4 +1,4 @@
-from harborapi.models.scanner import VulnerabilityItem, Severity
+from harborapi.models.scanner import Severity, VulnerabilityItem
 
 
 def test_vulnerabilityitem_get_severity_highest():
@@ -11,3 +11,16 @@ def test_vulnerabilityitem_get_severity_highest():
         },
     )
     assert vuln.get_severity_highest("Trivy", ("redhat", "nvd")) == Severity.critical
+
+
+def test_vulnerabilityitem_get_severity():
+    vuln = VulnerabilityItem(
+        vendor_attributes={
+            "CVSS": {
+                "nvd": {"V3Score": 7.5},
+                "redhat": {"V3Score": 9.1},
+            }
+        },
+    )
+    assert vuln.get_severity("Trivy", ("nvd", "redhat")) == Severity.high
+    assert vuln.get_severity("Trivy", ("redhat", "nvd")) == Severity.critical
