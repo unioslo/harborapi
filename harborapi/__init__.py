41dc9bd2dcbdee5cba281d32602b9f1e9da4e177
pederhan <pederhan@uio.no>
2022-11-18 12:02:14 +0100

    Add `ext` to __init__.py (+ __all__)

--- harborapi/__init__.py
+++ harborapi/__init__.py
@@ -1,6 +1,7 @@
 __version__ = "0.1.8"
 
+from . import ext
 from .client import HarborAsyncClient
 from .client_sync import HarborClient
 
-__all__ = ["HarborAsyncClient", "HarborClient"]
+__all__ = ["HarborAsyncClient", "HarborClient", "ext"]
