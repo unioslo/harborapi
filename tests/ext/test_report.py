5e9f421cc88a3db4c3e9952ab660eb537fd3aa42
pederhan <pederhan@uio.no>
2022-11-21 11:48:23 +0100

    Add further ArtifactReport tests

--- tests/ext/test_report.py
+++ tests/ext/test_report.py
@@ -1,3 +1,5 @@
+import copy
+
 from hypothesis import HealthCheck, assume, given, settings
 from hypothesis import strategies as st
 
@@ -47,4 +49,140 @@ def test_artifactreport(
         .artifacts
     ) == len(report.artifacts)
 
-    # test filtering by severity
+    # Make sure we have more than 1 artifact
+    # Use deep copy to ensure the artifacts are different objects
+    report.artifacts.extend(copy.deepcopy(report.artifacts))
+    assert report.is_aggregate
+    assert len(report.artifacts) > 1
+
+    # Test CVSS (with trivy vendor attributes)
+    # TODO: add other scanners
+    for artifact in report.artifacts:
+        artifact.report.scanner = "Trivy"
+        for vulnerability in artifact.report.vulnerabilities:
+            vulnerability.vendor_attributes = {
+                "CVSS": {
+                    "nvd": {"V3Score": 5.0, "V2Score": 5.0},
+                    "redhat": {"V3Score": 5.0, "V2Score": 5.0},
+                }
+            }
+    assert report.cvss.max == 5.0
+    assert report.cvss.min == 5.0
+    assert report.cvss.median == 5.0
+    assert report.cvss.mean == 5.0
+
+    # Get fixable/unfixable vulnerabilities
+    for is_fixable in [True, False]:
+        for artifact in report.artifacts:
+            for vulnerability in artifact.report.vulnerabilities:
+                if is_fixable:
+                    vulnerability.fix_version = "1.0.1"
+                else:
+                    vulnerability.fix_version = None
+                fixable = [v.vulnerability for v in report.fixable]
+                unfixable = [v.vulnerability for v in report.unfixable]
+                if is_fixable:
+                    assert vulnerability in fixable
+                    assert vulnerability not in unfixable
+                else:
+                    assert vulnerability not in fixable
+                    assert vulnerability in unfixable
+
+    # Test filtering by severity (+ show distribution)
+    n_vulnerabilities = sum(len(a.report.vulnerabilities) for a in report.artifacts)
+    for severity in Severity:
+        if not report.artifacts:
+            break
+        for i, artifact in enumerate(report.artifacts):
+            # Set the report and its vulnerabilities to the given severity
+            artifact.report.severity = severity
+            for vulnerability in artifact.report.vulnerabilities:
+                vulnerability.severity = severity
+            artifact.artifact.digest = f"{artifact.artifact.digest}-{i}-{severity}"
+        assert len(report.with_severity(severity).artifacts) == len(report.artifacts)
+        assert report.has_severity(severity)
+        # Test that the distribution is correct
+        # NOTE: report.distribution operates on vulnerabilities, not ArtifactInfo objects,
+        # which is inconsistent with the other methods. This should be renamed, and
+        # the method should be changed to operate on ArtifactInfo objects.
+        assert report.distribution[severity] == n_vulnerabilities
+
+
+@given(artifact_report_strategy)
+@settings(suppress_health_check=[HealthCheck.function_scoped_fixture])
+def test_artifactreport_with_repository(
+    report: ArtifactReport,
+) -> None:
+    assume(len(report.artifacts) > 0)
+    # It's unlikely hypothesis will generate the name "Test-Repo", but to ensure our test
+    # isn't flaky because of an assumption, we should set the names of _all_
+    # repositories every time.
+
+    N_ARTIFACTS = 10
+    report.artifacts = [copy.deepcopy(report.artifacts[0]) for i in range(N_ARTIFACTS)]
+    for i, artifact in enumerate(report.artifacts):
+        artifact.repository.name = "Test-Repo"
+        artifact.artifact.digest = f"{artifact.artifact.digest}-{i}"
+
+    assert report.has_repository("Test-Repo", case_sensitive=True)
+    assert report.has_repository("Test-Repo", case_sensitive=False)
+    assert report.has_repository("test-repo", case_sensitive=False)
+    assert not report.has_repository("test-repo", case_sensitive=True)
+
+    assert len(
+        report.with_repository("Test-Repo", case_sensitive=True).artifacts
+    ) == len(report.artifacts)
+    assert len(
+        report.with_repository("Test-Repo", case_sensitive=False).artifacts
+    ) == len(report.artifacts)
+    assert len(
+        report.with_repository("test-repo", case_sensitive=False).artifacts
+    ) == len(report.artifacts)
+    assert len(report.with_repository("test-repo", case_sensitive=True).artifacts) == 0
+
+    # Artifacts with different repos
+    report.artifacts[0].repository.name = "Test-Repo-2"
+    assert report.has_repository("Test-Repo-2", case_sensitive=True)
+    assert report.has_repository("Test-Repo-2", case_sensitive=False)
+    assert report.has_repository("test-repo-2", case_sensitive=False)
+    assert not report.has_repository("test-repo-2", case_sensitive=True)
+
+    assert (
+        len(report.with_repository("Test-Repo", case_sensitive=True).artifacts)
+        == len(report.artifacts) - 1
+    )
+    assert (
+        len(report.with_repository("Test-Repo", case_sensitive=False).artifacts)
+        == len(report.artifacts) - 1
+    )
+    assert (
+        len(report.with_repository("Test-Repo-2", case_sensitive=True).artifacts) == 1
+    )
+    assert (
+        len(report.with_repository("Test-Repo-2", case_sensitive=False).artifacts) == 1
+    )
+
+    # Match multiple repos
+    assert len(
+        report.with_repository(
+            ["Test-Repo", "Test-Repo-2"], case_sensitive=True
+        ).artifacts
+    ) == len(report.artifacts)
+    assert len(
+        report.with_repository(
+            ["Test-Repo", "Test-Repo-2"], case_sensitive=False
+        ).artifacts
+    ) == len(report.artifacts)
+    assert (
+        len(
+            report.with_repository(
+                ["test-repo", "test-repo-2"], case_sensitive=True
+            ).artifacts
+        )
+        == 0
+    )
+    assert len(
+        report.with_repository(
+            ["test-repo", "test-repo-2"], case_sensitive=False
+        ).artifacts
+    ) == len(report.artifacts)
