9845f51469e4ea8d76d443dd5f69d0b9a1e30943
pederhan <pederhan@usit.uio.no>
2023-01-17 12:12:40 +0100

    Bump version

--- harborapi/__init__.py
+++ harborapi/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.4.3"
+__version__ = "0.4.4"
 
 from . import _types, auth, client, client_sync, exceptions, utils, version
 from .client import HarborAsyncClient
--- tests/test_harborapi.py
+++ tests/test_harborapi.py
@@ -2,4 +2,4 @@ from harborapi import __version__
 
 
 def test_version():
-    assert __version__ == "0.4.3"
+    assert __version__ == "0.4.4"
