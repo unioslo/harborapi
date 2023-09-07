1c8d14a5e42fa54a8b570a94ebcee520c241a598
pederhan <pederhan@usit.uio.no>
2023-09-07 12:15:10 +0200

    AST parsing

--- codegen/ast/parser.py
+++ codegen/ast/parser.py
@@ -9,6 +9,14 @@ from typing import Any
 from typing import TypedDict
 
 
+class FragmentDir(str, Enum):
+    main = "main"
+    scanner = "scanner"
+
+    def __str__(self) -> str:
+        return self.value
+
+
 def construct_annotation(annotation: str | "ast.expr") -> "ast.expr":
     if isinstance(annotation, str):
         return ast.Name(id=annotation, ctx=ast.Load())
@@ -59,7 +67,10 @@ class Annotation(Modifier):
 
     def modify(self, node: ast.ClassDef) -> ast.ClassDef:
         for field in node.body:
-            if isinstance(field, ast.AnnAssign) and field.target.id == self.field:
+            if (
+                isinstance(field, ast.AnnAssign)
+                and getattr(field.target, "id", None) == self.field
+            ):
                 field.annotation = construct_annotation(self.annotation)
         return node
 
@@ -78,7 +89,10 @@ class AttrDocstring(Modifier):
     def modify(self, classdef: ast.ClassDef) -> ast.ClassDef:
         # FIXME: won't this just break when we modify while iterating?
         for idx, node in enumerate(classdef.body):
-            if not (isinstance(node, ast.Assign) and node.targets[0].id == self.attr):
+            if not (
+                isinstance(node, ast.Assign)
+                and getattr(node.targets[0], "id", None) == self.attr
+            ):
                 continue
 
             next_idx = idx + 1
@@ -138,9 +152,12 @@ class Field(Modifier):
         return kwargs
 
     def construct_field(self) -> ast.AnnAssign:
-        args = []
+        args = []  # type: list[str | ast.expr]
         if self.default is not Unset:
-            args.append(ast.Constant(value=self.default))
+            if isinstance(self.default, ast.expr):
+                args.append(self.default)
+            else:
+                args.append(ast.Constant(value=self.default))
         value = ast.Call(
             args=args,
             func=ast.Name(
@@ -160,119 +177,174 @@ class Field(Modifier):
 # TODO: sort this list
 # Changes to existing definitions (change annotation, etc.) or additions
 # that are too minor to create new fragments for.
-models = {
-    # "Artifact": [Annotation("scan_overview", "Optional[NativeReportSummary]")],
-    "ProjectMetadata": [Annotation("retention_id", "Optional[Union[str, int]]")],
-    "Error": [Docstring("Error response from Harbor.")],
-    "ScanOverview": [Docstring("Overview of scan results.")],
-    "VulnerabilitySummary": [Docstring("Summary of vulnerabilities found in a scan.")],
-    "CVEAllowlistItem": [Docstring("CVE allowlist item.")],
-    "RegistryProviderCredentialPattern": [
-        Docstring("Pattern for a registry credential.")
-    ],
-    "RegistryEndpoint": [Docstring("Registry endpoint configuration.")],
-    "FilterStyle": [Docstring("Style of the resource filter.")],
-    "ReplicationExecution": [Docstring("The execution of a replication job.")],
-    "ReplicationTask": [Docstring("A task that is a part of a replication job.")],
-    "RobotCreated": [Docstring("Response for robot account creation.")],
-    "RobotSec": [Docstring("Response for robot account secret refresh/update.")],
-    "Trigger": [Docstring("Trigger type for a 'scan all' job.")],
-    "Stats": [Docstring("Progress of the 'scan all' process.")],
-    "RetentionRuleParamMetadata": [Docstring("Parameters for a retention rule.")],
-    "RetentionSelectorMetadata": [Docstring("Metadata for a retention rule selector.")],
-    "Quota": [
-        Docstring("Quota object.")
-    ],  # not really sure how to succinctly describe this
-    "ScannerRegistration": [Docstring("A registered scanner adapter.")],
-    "ScannerAdapterMetadata": [Docstring("Metadata for a scanner adapter.")],
-    "LdapConf": [Docstring("LDAP configuration properties.")],
-    "LdapPingResult": [Docstring("Result of a ping to an LDAP server.")],
-    "PayloadFormat": [Docstring("Webhook payload format types.")],
-    "WebhookTargetObject": [Docstring("Webhook target")],
-    "WebhookPolicy": [Docstring("Webhook policy definition.")],
-    "WebhookLastTrigger": [
-        Docstring("Last trigger of the webhook and the event type of the trigger.")
-    ],
-    "WebhookJob": [Docstring("A webhook job.")],
-    "Parameter": [Docstring("Parameters for a 'scan all' policy.")],
-    "ComponentHealthStatus": [Docstring("Health status of a component.")],
-    "Accessory": [Docstring("Accessory of an artifact.")],
-    "ScanDataExportRequest": [Docstring("Criteria for selecting scan data to export.")],
-    "ScanDataExportJob": [Docstring("Metadata for a scan data export job.")],
-    "ScanDataExportExecution": [Docstring("Execution of a scan data export job.")],
-    "ScanDataExportExecutionList": [
-        Docstring("List of executed scan data export jobs.")
-    ],
-    "SearchRepository": [Docstring("Repository search result.")],
-    "WorkerPool": [Docstring("Worker pool for job service.")],
-    "Worker": [Docstring("Worker in a pool.")],
-    "Action": [
-        Docstring("Action to perform. Should be 'stop', 'pause', or 'resume'.")
-    ],  # Maybe don't hardcode these values in the docstring?
-    "ActionRequest": [Docstring("Request to perform an action.")],
-    "JobQueue": [Docstring("Information about a job queue.")],
-    "ScheduleTask": [Docstring("Information about a scheduled task.")],
-    "SchedulerStatus": [Docstring("Status of the scheduler.")],
-    "DangerousCVE": [Docstring("A CVE marked as dangerous.")],
-    "DangerousArtifact": [Docstring("An artifact marked as dangerous.")],
-    "VulnerabilityItem": [Docstring("Vulnerability found by a scan.")],
-    "Errors": [Docstring("Errors that occurred while handling a request.")],
-    "NativeReportSummary": [Docstring("Summary of a native scan report.")],
-    "CVEAllowlist": [Docstring("CVE allowlist for a system or project.")],
-    "RegistryInfo": [
-        Docstring("Registry information, including base info and capabilities.")
-    ],
-    "RegistryProviderEndpointPattern": [
-        Docstring("Pattern for a registry provider endpoint.")
-    ],
-    "RetentionRuleMetadata": [Docstring("Metadata for a tag retention rule.")],
-    "SupportedWebhookEventTypes": [
-        Docstring("Supported event and notification types for webhooks.")
-    ],
-    "OverallHealthStatus": [Docstring("Overall health status of the system.")],
-    "SecuritySummary": [Docstring("Artifact security summary.")],
-    "RegistryProviderInfo": [
-        Docstring(
-            "Registry provider information, including base info and capabilities."
-        )
-    ],
-    "RobotCreate": [Docstring("Request for robot account creation.")],
-    "RetentionMetadata": [Docstring("Metadata for a tag retention rule.")],
-    "RetentionPolicy": [Docstring("Retention policy.")],
-    "Type": [
-        AttrDocstring("none", "Cancel the schedule."),
-        AttrDocstring("manual", "Trigger schedule right away."),
-        AttrDocstring("schedule", "Trigger based on cron schedule."),
-    ],
-    "Schedule": [
-        Annotation("parameters", "Optional[Dict[str, Any]]"),
-    ],
-    # "GeneralInfo": [
-    #     Field(
-    #         "with_chartmuseum",
-    #         "Optional[bool]",
-    #         None,
-    #         description="DEPRECATED: Harbor instance is deployed with nested chartmuseum.",
-    #     ),
-    # ],
+models: dict[str, dict[str, list[Modifier]]] = {
+    FragmentDir.scanner: {
+        "HarborVulnerabilityReport": [
+            Field(
+                "generated_at",
+                "Optional[datetime]",
+                default=None,
+                description="The time the report was generated.",
+            ),
+            Field(
+                "artifact",
+                "Optional[Artifact]",
+                default=None,
+                description="The scanned artifact.",
+            ),
+            Field(
+                "scanner",
+                "Optional[Scanner]",
+                default=None,
+                description="The scanner used to generate the report.",
+            ),
+            Field(
+                "severity",
+                "Optional[Severity]",
+                default=None,
+                description="The overall severity of the vulnerabilities.",
+            ),
+            Field(
+                "vulnerabilities",
+                "List[VulnerabilityItem]",
+                default_factory=ast.keyword(
+                    arg="default_factory", value=ast.Name(id="list")
+                ),
+                description="The list of vulnerabilities found.",
+            ),
+        ],
+        "VulnerabilityItem": [
+            Field(
+                "severity",
+                "Severity",
+                default=ast.Attribute(
+                    value=ast.Name(id="Severity", ctx=ast.Load()),
+                    attr="unknown",
+                    ctx=ast.Load(),
+                ),
+                description="The severity of the vulnerability.",
+                example=ast.keyword(
+                    arg="example", value=ast.Name(id="Severity.high.value")
+                ),
+            ),
+        ],
+    },
+    FragmentDir.main: {
+        "ProjectMetadata": [Annotation("retention_id", "Optional[Union[str, int]]")],
+        "ExtraAttrs": [Annotation("root", "Optional[Dict[str, Any]]")],
+        "Error": [Docstring("Error response from Harbor.")],
+        "ScanOverview": [Docstring("Overview of scan results.")],
+        "VulnerabilitySummary": [
+            Docstring("Summary of vulnerabilities found in a scan.")
+        ],
+        "CVEAllowlistItem": [Docstring("CVE allowlist item.")],
+        "RegistryProviderCredentialPattern": [
+            Docstring("Pattern for a registry credential.")
+        ],
+        "RegistryEndpoint": [Docstring("Registry endpoint configuration.")],
+        "FilterStyle": [Docstring("Style of the resource filter.")],
+        "ReplicationExecution": [Docstring("The execution of a replication job.")],
+        "ReplicationTask": [Docstring("A task that is a part of a replication job.")],
+        "RobotCreated": [Docstring("Response for robot account creation.")],
+        "RobotSec": [Docstring("Response for robot account secret refresh/update.")],
+        "Trigger": [Docstring("Trigger type for a 'scan all' job.")],
+        "Stats": [Docstring("Progress of the 'scan all' process.")],
+        "RetentionRuleParamMetadata": [Docstring("Parameters for a retention rule.")],
+        "RetentionSelectorMetadata": [
+            Docstring("Metadata for a retention rule selector.")
+        ],
+        "Quota": [
+            Docstring("Quota object.")
+        ],  # not really sure how to succinctly describe this
+        "ScannerRegistration": [Docstring("A registered scanner adapter.")],
+        "ScannerAdapterMetadata": [Docstring("Metadata for a scanner adapter.")],
+        "LdapConf": [Docstring("LDAP configuration properties.")],
+        "LdapPingResult": [Docstring("Result of a ping to an LDAP server.")],
+        "PayloadFormat": [Docstring("Webhook payload format types.")],
+        "WebhookTargetObject": [Docstring("Webhook target")],
+        "WebhookPolicy": [Docstring("Webhook policy definition.")],
+        "WebhookLastTrigger": [
+            Docstring("Last trigger of the webhook and the event type of the trigger.")
+        ],
+        "WebhookJob": [Docstring("A webhook job.")],
+        "Parameter": [Docstring("Parameters for a 'scan all' policy.")],
+        "ComponentHealthStatus": [Docstring("Health status of a component.")],
+        "Accessory": [Docstring("Accessory of an artifact.")],
+        "ScanDataExportRequest": [
+            Docstring("Criteria for selecting scan data to export.")
+        ],
+        "ScanDataExportJob": [Docstring("Metadata for a scan data export job.")],
+        "ScanDataExportExecution": [Docstring("Execution of a scan data export job.")],
+        "ScanDataExportExecutionList": [
+            Docstring("List of executed scan data export jobs.")
+        ],
+        "SearchRepository": [Docstring("Repository search result.")],
+        "WorkerPool": [Docstring("Worker pool for job service.")],
+        "Worker": [Docstring("Worker in a pool.")],
+        "Action": [
+            Docstring("Action to perform. Should be 'stop', 'pause', or 'resume'.")
+        ],  # Maybe don't hardcode these values in the docstring?
+        "ActionRequest": [Docstring("Request to perform an action.")],
+        "JobQueue": [Docstring("Information about a job queue.")],
+        "ScheduleTask": [Docstring("Information about a scheduled task.")],
+        "SchedulerStatus": [Docstring("Status of the scheduler.")],
+        "DangerousCVE": [Docstring("A CVE marked as dangerous.")],
+        "DangerousArtifact": [Docstring("An artifact marked as dangerous.")],
+        "VulnerabilityItem": [Docstring("Vulnerability found by a scan.")],
+        "Errors": [Docstring("Errors that occurred while handling a request.")],
+        "NativeReportSummary": [Docstring("Summary of a native scan report.")],
+        "CVEAllowlist": [Docstring("CVE allowlist for a system or project.")],
+        "RegistryInfo": [
+            Docstring("Registry information, including base info and capabilities.")
+        ],
+        "RegistryProviderEndpointPattern": [
+            Docstring("Pattern for a registry provider endpoint.")
+        ],
+        "RetentionRuleMetadata": [Docstring("Metadata for a tag retention rule.")],
+        "SupportedWebhookEventTypes": [
+            Docstring("Supported event and notification types for webhooks.")
+        ],
+        "OverallHealthStatus": [Docstring("Overall health status of the system.")],
+        "SecuritySummary": [Docstring("Artifact security summary.")],
+        "RegistryProviderInfo": [
+            Docstring(
+                "Registry provider information, including base info and capabilities."
+            )
+        ],
+        "RobotCreate": [Docstring("Request for robot account creation.")],
+        "RetentionMetadata": [Docstring("Metadata for a tag retention rule.")],
+        "RetentionPolicy": [Docstring("Retention policy.")],
+        "Type": [
+            AttrDocstring("none", "Cancel the schedule."),
+            AttrDocstring("manual", "Trigger schedule right away."),
+            AttrDocstring("schedule", "Trigger based on cron schedule."),
+        ],
+        "Schedule": [
+            Annotation("parameters", "Optional[Dict[str, Any]]"),
+        ],
+        # "GeneralInfo": [
+        #     Field(
+        #         "with_chartmuseum",
+        #         "Optional[bool]",
+        #         None,
+        #         description="DEPRECATED: Harbor instance is deployed with nested chartmuseum.",
+        #     ),
+        # ],
+    },
 }
 
 
-def modify_module(tree: ast.Module) -> ast.Module:
+def modify_module(tree: ast.Module, fragment_dir: FragmentDir) -> ast.Module:
     for node in ast.walk(tree):
         # Check if the node is a ClassDef and the name is 'Foo'
-        if isinstance(node, ast.ClassDef) and node.name in models:
-            for modifier in models[node.name]:
+        if isinstance(node, ast.ClassDef) and node.name in models[fragment_dir]:
+            for modifier in models[fragment_dir][node.name]:
                 node = modifier.modify(node)
     return tree
 
 
-ADD_IMPORTS = {
-    "pydantic": ["model_validator"],
-    "typing": ["Tuple", "Union"],
-    ".scanner": ["Severity"],
-    "..log": ["logger"],
-}
+# Imports that should be added to every file
+ADD_IMPORTS = {}  # type: dict[str, list[str]] # module: list[import_name]
 
 
 def add_imports(tree: ast.Module) -> ast.Module:
@@ -388,22 +460,17 @@ def extract_statements(fragment_ast: ast.Module) -> StatementDict:
 
 def add_fragments(tree: ast.Module, directory: Path) -> ast.Module:
     classdefs = {}  # type: dict[str, ast.ClassDef]
+    statements = StatementDict(imports=[], stmts=[])
     for file in Path(directory).iterdir():
         if file.suffix == ".py":
             fragment_ast = ast.parse(file.read_text())
             classdefs.update(extract_classdefs(fragment_ast))
-            statements = extract_statements(fragment_ast)
+            stmts = extract_statements(fragment_ast)
+            statements["imports"].extend(stmts["imports"])
+            statements["stmts"].extend(stmts["stmts"])
     new_tree = insert_or_update_classdefs(tree, classdefs)
     new_tree = insert_statements(new_tree, statements)
     return new_tree
-
-
-class FragmentDir(str, Enum):
-    main = "main"
-    scanner = "scanner"
-
-    def __str__(self) -> str:
-        return self.value
 
 
 if __name__ == "__main__":
@@ -434,7 +501,7 @@ if __name__ == "__main__":
     tree = ast.parse(source)
 
     # Modify the AST
-    new_tree = modify_module(tree)
+    new_tree = modify_module(tree, directory)
     new_tree = add_imports(new_tree)
     new_tree = add_fragments(new_tree, fragment_dir)
 
