fe9b7c7216933cc9f5aa66e1af28b2d3e568c804
pederhan <pederhan@usit.uio.no>
2023-09-08 11:49:39 +0200

    Use basic RootModel fro RegistryProviders

--- codegen/ast/fragments/main/registryproviders.py
+++ codegen/ast/fragments/main/registryproviders.py
@@ -6,18 +6,10 @@ from __future__ import annotations
 from typing import Dict
 
 from pydantic import Field
-from pydantic import RootModel
 
 
-class RegistryProviders(RootModel[Dict[str, RegistryProviderInfo]]):
+class RegistryProviders(RootModel):
     root: Dict[str, RegistryProviderInfo] = Field(
         {},
         description="The registry providers. Each key is the name of the registry provider.",
     )
-
-    @property
-    def providers(self) -> Dict[str, RegistryProviderInfo]:
-        return self.root
-
-    def __getitem__(self, key: str) -> RegistryProviderInfo:
-        return self.root[key]
