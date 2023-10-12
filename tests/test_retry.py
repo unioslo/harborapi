f30fcb8a936e6aa37d5531a3dc17bd64cb2a9cdc
pederhan <pederhan@usit.uio.no>
2023-10-12 12:49:34 +0200

    Add retry failure path tests

--- tests/test_retry.py
+++ tests/test_retry.py
@@ -11,6 +11,7 @@ from harborapi.client import HarborAsyncClient
 from harborapi.exceptions import HarborAPIException
 from harborapi.exceptions import StatusError
 from harborapi.retry import get_backoff_kwargs
+from harborapi.retry import retry
 from harborapi.retry import RetrySettings
 
 
@@ -219,3 +220,31 @@ async def test_no_retry_ctx_manager(
     assert isinstance(users, list)
     assert len(users) == 2
     assert async_client.retry.enabled
+
+
+def test_retry_decorator_on_function() -> None:
+    """Applying the retry decorator on a function without arguments
+    should raise a ValueError."""
+    with pytest.raises(ValueError) as exc_info:
+
+        @retry()
+        def foo():
+            pass
+
+        foo()
+    assert "method" in str(exc_info.value)
+
+
+def test_retry_decorator_on_other_class() -> None:
+    """Applying the retry decorator on other classes than HarborAsyncClient
+    should raise a TypeError."""
+    with pytest.raises(TypeError) as exc_info:
+
+        class Foo:
+            @retry()
+            def foo():
+                pass
+
+        f = Foo()
+        f.foo()
+    assert "HarborAsyncClient" in str(exc_info.value)
