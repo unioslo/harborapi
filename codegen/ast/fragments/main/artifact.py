9d801f623fde58dee787524070b6fefd900227c3
pederhan <pederhan@usit.uio.no>
2023-10-17 15:25:07 +0200

    Explain MIME type in `Artifact.scan`

--- codegen/ast/fragments/main/artifact.py
+++ codegen/ast/fragments/main/artifact.py
@@ -4,11 +4,13 @@ from __future__ import annotations
 class Artifact(BaseModel):
     @property
     def scan(self) -> Optional[NativeReportSummary]:
-        """Returns the first scan overview found for the Artifact,
+        """
+        Returns the first scan overview found for the Artifact,
         or None if there are none.
 
-        In most cases an Artifact will only have one scan overview, and
-        in those cases, this is a shortcut to access it.
+        Artifacts are typically scanned in a single format, represented
+        by its MIME type. Thus, most Artifacts will have only one
+        scan overview. This property provides a quick access to it.
         """
         if self.scan_overview and self.scan_overview.root:
             return self.scan_overview.root[next(iter(self.scan_overview))]
