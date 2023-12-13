6b50d1c5621923adc154023911861e7bfd043277
pederhan <pederhan@usit.uio.no>
2023-12-13 11:15:49 +0100

    Remove unused type

--- harborapi/_types.py
+++ harborapi/_types.py
@@ -5,17 +5,11 @@ from typing import Dict
 from typing import List
 from typing import MutableMapping
 from typing import Sequence
-from typing import TypeVar
 from typing import Union
 
 from httpx._types import PrimitiveData
-from pydantic import BaseModel
 
 JSONType = Union[Dict[str, Any], List[Any]]  # TODO: Use PrimitiveData
-
-T = TypeVar("T", bound=BaseModel)
-ModelOrDict = Union[T, Dict[str, Any]]
-"""A type that can be either a model or a dict."""
 
 
 # HTTP(X)
