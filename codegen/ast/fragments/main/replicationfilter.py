30c5172891bcd7e7fd0218b704bb317ee4a88a2e
pederhan <pederhan@usit.uio.no>
2024-03-01 11:34:14 +0100

    Fix ReplicationFilter.value failing to validate

---
+++ codegen/ast/fragments/main/replicationfilter.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+from typing import Any
+from typing import Dict
+from typing import Union
+
+from pydantic import Field
+
+
+class ReplicationFilter(BaseModel):
+    value: Union[str, Dict[str, Any], None] = Field(
+        None, description="The value of replication policy filter."
+    )
